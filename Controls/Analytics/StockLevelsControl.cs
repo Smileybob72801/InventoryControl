d10039d871cf589af1c00aee3ee349b809e0040d
Daniel <Smileybob72801@gmail.com>
2025-01-27 07:48:54 -0600

    Add stock levels control

---
+++ Controls/Analytics/StockLevelsControl.cs
@@ -0,0 +1,28 @@
+﻿using InventoryControl.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventoryControl.Controls.Analytics
+{
+	public partial class StockLevelsControl : UserControl
+	{
+		public StockLevelsControl()
+		{
+			InitializeComponent();
+		}
+
+		private void StockLevelsControl_Load(object sender, EventArgs e)
+		{
+			using NorthwindContext context = new();
+
+
+		}
+	}
+}
